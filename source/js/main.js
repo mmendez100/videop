77a573f2617c73dbadfa0a76625f8993f0784864
mmendez <mmendez100@gmail.com>
2016-01-16 00:20:58 -0500

    added js file missing from last commmit

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+////////////////// Main ////////////////////
+
+
+// We attach an instance of class Videop to the existing video player.
+// specifying the playbar's CSS style and its size or ratio ("thickness")
+var videoPlayer1 = new Videop("video1", "playbar", "#FFD700", "#8B0000", 0.08);
+
+// We attach an instance of class Videop to the existing video player.
+// specifying the playbar's CSS style and its size or ratio ("thickness")
+//var videoPlayer2 = new Videop("video2", "playbar", "#FFD700", "#8B0000", 0.08);
